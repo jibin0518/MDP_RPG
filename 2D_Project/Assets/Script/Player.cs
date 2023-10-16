871952fdb18f7969ad3de91f534f7201c72fe90e
jibin0518 <114150533+jibin0518@users.noreply.github.com>
2023-10-16 18:52:54 +0900

    20231016

---
+++ 2D_Project/Assets/Script/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputvec;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    public float spd;
+    public float run;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputvec.x = Input.GetAxisRaw("Horizontal");
+        inputvec.y = Input.GetAxisRaw("Vertical");
+        run = Input.GetKey(KeyCode.LeftShift) ? 1.3f : 1;
+
+        if(Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == 1;
+        if (rigid.velocity.normalized.x == 0)
+            anim.SetBool("Test", false);
+        else
+            anim.SetBool("Test", true);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextvec = inputvec.normalized * spd * Time.fixedDeltaTime * run;
+        rigid.MovePosition(rigid.position + nextvec);
+    }
+}
