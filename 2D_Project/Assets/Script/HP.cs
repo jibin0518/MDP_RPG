e9867cfbfb461114b26f7417ae984ac6f9f39fbc
kme0227 <kko20_s23_10204@gclass.ice.go.kr>
2023-10-17 21:13:40 +0900

    20231017
    Hpbar

---
+++ 2D_Project/Assets/Script/HP.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /*[SerializeField]
+     private Slider hpbar;
+     private float maxhp = 100;
+     private float curhp = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpbar.value = (float) curhp / (float) maxhp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space)) 
+        {
+            curhp -= 20;
+        }
+    }
+    private void Handlehp;
+    {
+        hpbar.value = (float)curhp / (float) maxhp;
+    }*/
+}
