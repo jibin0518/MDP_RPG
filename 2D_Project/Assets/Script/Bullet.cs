f80b7c59060ed0e58ff846dc982ee6e1517ce056
jibin0518 <114150533+jibin0518@users.noreply.github.com>
2023-11-21 20:20:11 +0900

    20231121

--- 2D_Project/Assets/Script/Bullet.cs
+++ 2D_Project/Assets/Script/Bullet.cs
@@ -8,17 +8,26 @@ public class Bullet : MonoBehaviour
     public float speed = 10f;  // 미사일 이동 속도
     private float lifetime = 1.5f;  // 미사일 수명 (초)
     private float spawnTime;  // 미사일 생성 시간
-    //HeroKnight Player;
+    public int bulletDirection;
+    HeroKnight player;
 
     void Start()
     {
-        //Player = GetComponent<HeroKnight>();
+        player = GetComponent<HeroKnight>();
         spawnTime = Time.time;  // 현재 시간을 저장
     }
 
     void Update()
     {
-        transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+        if(player.GetComponent<SpriteRenderer>().flipX == true)
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(transform.right * -1 * speed * Time.deltaTime);
+        }
 
         // 미사일의 수명이 다 되면 삭제
         if (Time.time - spawnTime > lifetime)
